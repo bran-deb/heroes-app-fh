61dc562b4b65a9d859b089090cae7da7a7fa8859
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-13 17:38:35 -0400

    replace from useState

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export const LoginScreen = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        //remplaza la ruta anterior por la ruta de destino
+        navigate("/", { replace: true });
+    }
+
     return (
-        <div>
+        <div className='container mt-5'>
             <h1>LoginScreen</h1>
+            <hr />
+
+            <button
+                className='btn btn-primary'
+                onClick={handleLogin}
+            >
+                login
+            </button>
         </div>
     )
 }
