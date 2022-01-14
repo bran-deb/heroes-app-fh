063265b01e3715a4e9dcd91097a83f649c29021a
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-13 21:30:11 -0400

    cards were added

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { getHeroByPublisher } from '../../selectors/getHHeroesByPublisher'
+import { HeroCard } from './HeroCard'
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroByPublisher(publisher)
 
     return (
-        <ul>
+        <div className="row row-cols-1 row-cols-md-3 g-3">
             {
                 heroes.map(hero => (
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard
+                        key={hero.id}
+                        {...hero}
+                    />
                 ))
             }
-        </ul>
+        </div>
     )
 }
