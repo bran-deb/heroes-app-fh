60b9000ccb56ae6a0b54229e252d066efeb4a508
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-20 21:18:40 -0400

    update tests dashboardroutes

--- src/tests/routers/DashboardRoutes.test.js
+++ src/tests/routers/DashboardRoutes.test.js
@@ -13,17 +13,32 @@ describe('tests en <DashboardRoutes/>', () => {
         }
     }
 
-    test('debe de mostrarse correctamente', () => {
+    test('debe de mostrarse correctamente - Marvel', () => {
         const wrapper = mount(
             <AuthContext.Provider value={contextValue}>
-                <MemoryRouter>
+                <MemoryRouter initialEntries={['/']}>
                     <DashboardRoutes />
                 </MemoryRouter>
             </AuthContext.Provider>
         )
         const span = wrapper.find('.text-info')
+        const h1 = wrapper.find('h1')
         expect(wrapper).toMatchSnapshot()
         expect(span.text().trim()).toBe('Juanes')
+        expect(h1.text().trim()).toBe('MarvelScreen')
+    })
+
+    test('debe de mostrarse correctamente -Dc', () => {
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/dc']}>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        )
+        const h1 = wrapper.find('h1')
+        expect(wrapper).toMatchSnapshot()
+        expect(h1.text().trim()).toBe('DC Screen')
     })
 
 })
